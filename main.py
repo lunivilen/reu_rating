83479bc8b3860acb31b5d8857218558807e5a12a
lunivilen <ilya24624864@gmail.com>
2022-08-05 20:55:07 +0300

    New features, include profile and semester selection

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
+import json
 from selenium.common.exceptions import NoSuchElementException
 from selenium import webdriver
 from bs4 import BeautifulSoup as bs
 import os
 
 
-def get_rating(login, password, semester=0):
+def get_rating(login, password):
     final_return_file = []
 
     # urls
     url_main_page = "https://student.rea.ru/"
-    if semester:
-        url_rating = f"https://student.rea.ru/rating/index.php?semester={semester}-й+семестр"
-    else:
-        url_rating = "https://student.rea.ru/rating/index.php"
 
     # Option
     option = webdriver.ChromeOptions()
@@ -30,94 +27,116 @@ def get_rating(login, password, semester=0):
     try:
         print(soup.find(class_="errortext").text)
     except AttributeError:
-        profile = 0
+        pass
 
-        # Parsing all profiles
+    profile_amount = 0
+    try:
+        browser.find_element("xpath", "/html/body/nav/div/div/div[1]/a").click()
+        soup = bs(markup=browser.page_source, features="lxml")
+        profile_amount = len(list(map(lambda x: x.text, soup.find_all(class_="fakultet__selector_profile"))))
+
+        with open("temp_file.json", "w", encoding="utf-8") as f:
+            json.dump(profile_amount, f)
+    except NoSuchElementException:
+        pass
+
+    if profile_amount:
         while True:
-            profile += 1
             try:
-                # Trying to choose next profile
-                browser.find_element("xpath", "/html/body/nav/div/div/div[1]/a").click()
+                profile = int(input(f"Ведите номер одного из {profile_amount} профилей\n"))
                 browser.find_element("xpath",
                                      f"/html/body/div[7]/div[2]/div[4]/div/div/div[3]/div[{profile}]/label").click()
                 browser.find_element("xpath", "/html/body/div[7]/div[2]/div[4]/div/div/button[1]").click()
-                if profile == 1:
-                    browser.get(url=url_rating)
-                src = browser.page_source
-
-                # Save locally site's page
-                with open(f"rating_{profile}.html", "w", encoding="utf-8") as f:
-                    f.write(src)
-
-            except NoSuchElementException:
-                if profile == 1:
-                    profile += 1
-                    browser.get(url=url_rating)
-                    src = browser.page_source
-
-                    # Save locally site's page
-                    with open("rating_1.html", "w", encoding="utf-8") as f:
-                        f.write(src)
                 break
-        browser.quit()
+            except ValueError:
+                print("Выбраный вами профиль не существует")
 
-        # Getting table of scores
-        for k in range(1, profile):
-            with open(f"rating_{k}.html", "r", encoding="utf-8") as f:
-                src = f.read()
-            soup = bs(markup=src, features="lxml")
+    soup = bs(markup=browser.page_source, features="lxml")
+    prof_n = soup.find(class_="breadcrumb__fakultet__popup").text
+    semester_amount = int(prof_n[29]) * 2
 
-            try:
-                head_list = list(map(lambda x: x.text, soup.find(class_="es-rating__line")))
-                head_list = list(filter(lambda x: x != "\n", head_list))
+    with open("temp_file.json", "w", encoding="utf-8") as f:
+        json.dump(semester_amount, f)
 
-                subjects = list(
-                    map(lambda x: x.text, soup.find_all(class_="es-rating__line-item es-rating__discipline")))
-                subjects.pop(0)
-                subjects_rows = []
-                for i in subjects:
-                    b = list(filter(lambda x: x != "", i.split(" ")))
-                    b.pop(0)
-                    b = " ".join(b)
-                    subjects_rows.append([b])
+    while True:
+        try:
+            semester = int(input(
+                f"Введите номер одного из {semester_amount} семестров или 0, если хотите посмотреть рейтинг за все семестры\n"))
+            if -1 < semester <= semester_amount:
+                break
+        except ValueError:
+            print("Выбранный вами семетр не существует")
+    if semester:
+        prof_n = prof_n[29:prof_n.index(',') + 1] + prof_n[62:-8] + f", {semester}-й семестр"
+        url_rating = f"https://student.rea.ru/rating/index.php?semester={semester}-й+семестр"
+    else:
+        prof_n = prof_n[29:prof_n.index(',') + 1] + prof_n[62:-8]
+        url_rating = "https://student.rea.ru/rating/index.php"
 
-                # Checking for course work
-                is_course_work = soup.find_all(class_="es-rating__line es-rating__line-child close")
-                if is_course_work != "":
-                    course_work = []
-                    for i in is_course_work:
-                        course_work.append(i.find(class_="es-rating__line-item es-rating__discipline").text)
+    browser.get(url=url_rating)
+    src = browser.page_source
 
-                    course_work_subject = []
-                    for i in course_work:
-                        b = list(filter(lambda x: x != "", i.split(" ")))
-                        b.pop(0)
-                        b = " ".join(b)
-                        course_work_subject.append(b)
-                    for i in range(len(subjects_rows) - 1):
-                        if subjects_rows[i] == subjects_rows[i + 1] and subjects_rows[i][0] in course_work_subject:
-                            course_work_subject.remove(subjects_rows[i][0])
-                            subjects_rows[i + 1][0] += "(Курсовая работа или пересдача)"
+    # Save locally site's page
+    with open(f"rating.html", "w", encoding="utf-8") as f:
+        f.write(src)
+    os.remove("temp_file.json")
+    browser.quit()
 
-                scores = list(map(lambda x: x.text, soup.find_all(class_="es-rating__tab-body-item")))
-                scores = list(filter(lambda x: x != "", scores[0].split(" ")))
-                scores = list(filter(lambda x: x[0].isdigit(), scores))
-                scores_rows = [scores[i:i + 5] for i in range(0, len(scores), 5)]
+    # Getting table of scores
+    with open(f"rating.html", "r", encoding="utf-8") as f:
+        src = f.read()
+    soup = bs(markup=src, features="lxml")
 
-                # Generate dict for json file
-                temp_dict = []
-                final_rows_json = []
-                for i in range(len(scores_rows)):
-                    temp_dict.append(dict(zip(head_list[1:], scores_rows[i])))
+    try:
+        head_list = list(map(lambda x: x.text, soup.find(class_="es-rating__line")))
+        head_list = list(filter(lambda x: x != "\n", head_list))
 
-                for i in range(len(temp_dict)):
-                    final_rows_json.append(dict.fromkeys(subjects_rows[i], temp_dict[i]))
+        subjects = list(
+            map(lambda x: x.text, soup.find_all(class_="es-rating__line-item es-rating__discipline")))
+        subjects.pop(0)
+        subjects_rows = []
+        for i in subjects:
+            b = list(filter(lambda x: x != "", i.split(" ")))
+            b.pop(0)
+            b = " ".join(b)
+            subjects_rows.append([b])
 
-                a = soup.find(class_="breadcrumb__fakultet__popup").text
-                final_return_file.append([a[29:a.index(',') + 1] + a[62:-8], *final_rows_json])
-                os.remove(f"rating_{k}.html")
-            except TypeError:
-                a = soup.find(class_="breadcrumb__fakultet__popup").text
-                final_return_file.append([a[29:a.index(',') + 1] + a[62:-8], "Рейтинга нет"])
-                os.remove(f"rating_{k}.html")
+        # Checking for course work
+        is_course_work = soup.find_all(class_="es-rating__line es-rating__line-child close")
+        if is_course_work != "":
+            course_work = []
+            for i in is_course_work:
+                course_work.append(i.find(class_="es-rating__line-item es-rating__discipline").text)
+
+            course_work_subject = []
+            for i in course_work:
+                b = list(filter(lambda x: x != "", i.split(" ")))
+                b.pop(0)
+                b = " ".join(b)
+                course_work_subject.append(b)
+            for i in range(len(subjects_rows) - 1):
+                if subjects_rows[i] == subjects_rows[i + 1] and subjects_rows[i][0] in course_work_subject:
+                    course_work_subject.remove(subjects_rows[i][0])
+                    subjects_rows[i + 1][0] += "(Курсовая работа или пересдача)"
+
+        scores = list(map(lambda x: x.text, soup.find_all(class_="es-rating__tab-body-item")))
+        scores = list(filter(lambda x: x != "", scores[0].split(" ")))
+        scores = list(filter(lambda x: x[0].isdigit(), scores))
+        scores_rows = [scores[i:i + 5] for i in range(0, len(scores), 5)]
+
+        # Generate dict for json file
+        temp_dict = []
+        final_rows_json = []
+        for i in range(len(scores_rows)):
+            temp_dict.append(dict(zip(head_list[1:], scores_rows[i])))
+
+        for i in range(len(temp_dict)):
+            final_rows_json.append(dict.fromkeys(subjects_rows[i], temp_dict[i]))
+
+        final_return_file.append({prof_n: final_rows_json})
+        os.remove(f"rating.html")
+    except TypeError:
+        final_return_file.append({prof_n: None})
+        os.remove(f"rating.html")
+
     return final_return_file
